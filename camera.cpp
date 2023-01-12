5a30f5fa02a0b03d38df984e7f97eee4d2415689
AiraOomiya <221007@st.yoshida-g.ac.jp>
2023-01-12 11:49:11 +0900

    カメラのマクロ追加

--- camera.cpp
+++ camera.cpp
@@ -10,16 +10,16 @@ Author:大宮愛羅
 #include "player.h"
 
 //注視点情報
-#define POSR_SPEED	(10.0f)		//注視点の移動速度
-#define POSR_WIDTH	(0.0f)		//注視点の幅
-#define POSR_HEIGHT	(0.0f)		//注視点の高さ
-#define POSR_DEPTH	(0.0f)		//注視点の奥行き
+#define POSR_SPEED	(10.0f)			//注視点の移動速度
+#define POSR_WIDTH	(0.0f)			//注視点の幅
+#define POSR_HEIGHT	(0.0f)			//注視点の高さ
+#define POSR_DEPTH	(0.0f)			//注視点の奥行き
 
 //視点情報
 #define POSV_ROTSPEED	(0.05f)		//視点の回転速度
 #define POSV_SPEED		(10.0f)		//視点の移動速度
-#define POSV_DISTANCE	(1000)		//視点の距離
 #define POSV_ADD		(400)		//視点の上下移動
+#define POSV_DISTANCE	(1000)		//視点の距離
 #define POSV_WIDTH		(0.0f)		//視点の幅
 #define POSV_HEIGHT		(500.0f)	//視点の高さ
 #define POSV_DEPTH		(0.0f)		//視点の奥行き
@@ -29,8 +29,16 @@ Author:大宮愛羅
 #define VECU_HEIGHT		(1.0f)		//上方向ベクトルの高さ
 #define VECV_DEPTH		(0.0f)		//上方向ベクトルの奥行き
 
+//カメラ情報
+#define CAMERA_ROT		(45.0f)		//カメラの向き
+#define CAMERA_POS		(10.0f)		//カメラの位置
+
 //描画範囲
-#define DRAWING_AREA	(50000.0f)
+#define DRAWING_AREA	(50000.0f)	//最大描画範囲
+
+//位置の差分を2倍
+#define DIFF_TIMES		(2.0f)		//2乗
+
 
 //グローバル変数
 Camera g_Camera;						//カメラの情報
@@ -40,27 +48,27 @@ float  g_MaxDrawCamera = DRAWING_AREA;	//最大描画範囲
 void InitCamera(void)
 {
 	g_Camera.posV = D3DXVECTOR3(POSV_WIDTH, POSV_HEIGHT, POSV_DEPTH);	//視点
-	g_Camera.posR = D3DXVECTOR3(POSR_WIDTH, POSR_HEIGHT, POSR_DEPTH);
-	g_Camera.vecU = D3DXVECTOR3(VECU_WIDTH, VECU_HEIGHT, VECV_DEPTH);
+	g_Camera.posR = D3DXVECTOR3(POSR_WIDTH, POSR_HEIGHT, POSR_DEPTH);	//注視点
+	g_Camera.vecU = D3DXVECTOR3(VECU_WIDTH, VECU_HEIGHT, VECV_DEPTH);	//上方向ベクトル
 
 	Player * pPlayer = GetPlayer();
 
 	//視点
-	g_Camera.posV.x = pPlayer->pos.x;
-	g_Camera.posV.y = pPlayer->pos.y + POSV_ADD;
-	g_Camera.posV.z = pPlayer->pos.z - POSV_DISTANCE;
+	g_Camera.posV.x = pPlayer->pos.x;					//視点X
+	g_Camera.posV.y = pPlayer->pos.y + POSV_ADD;		//視点Y
+	g_Camera.posV.z = pPlayer->pos.z - POSV_DISTANCE;	//視点Z
 
 	//注視点
-	g_Camera.posR.x = pPlayer->pos.x;
-	g_Camera.posR.y = pPlayer->pos.y;
-	g_Camera.posR.z = pPlayer->pos.z;
+	g_Camera.posR.x = pPlayer->pos.x;		//注視点X
+	g_Camera.posR.y = pPlayer->pos.y;		//注視点Y
+	g_Camera.posR.z = pPlayer->pos.z;		//注視点Z
 
 	//それぞれの位置の差分を格納する変数
 	float PosDiffX, PosDiffY, PosDiffZ;
 
-	PosDiffX = pow(g_Camera.posR.x - g_Camera.posV.x, 2.0f);	//２乗
-	PosDiffY = pow(g_Camera.posR.y - g_Camera.posV.y, 2.0f);	//２乗
-	PosDiffZ = pow(g_Camera.posR.z - g_Camera.posV.z, 2.0f);	//２乗
+	PosDiffX = pow(g_Camera.posR.x - g_Camera.posV.x, DIFF_TIMES);	//２乗
+	PosDiffY = pow(g_Camera.posR.y - g_Camera.posV.y, DIFF_TIMES);	//２乗
+	PosDiffZ = pow(g_Camera.posR.z - g_Camera.posV.z, DIFF_TIMES);	//２乗
 
 	//長さの算出
 	g_Camera.fLength = sqrtf(PosDiffX + PosDiffY + PosDiffZ);
@@ -88,7 +96,7 @@ void SetCamera(void)
 	D3DXMatrixIdentity(&g_Camera.mtxProjection);
 
 	//プロジェクションマトリックスの作成
-	D3DXMatrixPerspectiveFovLH(&g_Camera.mtxProjection, D3DXToRadian(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 10.0f, g_MaxDrawCamera);
+	D3DXMatrixPerspectiveFovLH(&g_Camera.mtxProjection, D3DXToRadian(CAMERA_ROT), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, CAMERA_POS, g_MaxDrawCamera);
 
 	//プロジェクションマトリックスの設定
 	pDevice->SetTransform(D3DTS_PROJECTION, &g_Camera.mtxProjection);
