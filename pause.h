62e5d5c7c35aee3715af44c9f0052c160622ee17
RyukiFuziwara <221069@st.yoshida-g.ac.jp>
2023-01-13 10:17:30 +0900

    ハンバーグ

---
+++ pause.h
@@ -0,0 +1,18 @@
+//==========================================
+//
+//ポーズのヘッダ[pause.h]
+//Author:藤原龍輝
+//
+//==========================================
+#ifndef _PAUSE_H_
+#define _PAUSE_H_
+
+//****************************//
+//		プロトタイプ宣言	  //
+//****************************//
+void InitPause(void);
+void UninitPause(void);
+void UpdatePause(void);
+void DrawPause(void);
+
+#endif 
