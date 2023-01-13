ef0c3fb5c7645e640e1ae85b72d55ca68b61a0ca
AiraOomiya <221007@st.yoshida-g.ac.jp>
2023-01-13 13:17:17 +0900

    カメラの移動処理を追加

--- camera.h
+++ camera.h
@@ -23,10 +23,11 @@ typedef struct
 } Camera;
 
 //プロトタイプ宣言
-void InitCamera(void); //初期化処理
-void UninitCamera(void); //終了処理
-void UpdateCamera(void); //更新処理
-void SetCamera(void); //設定処理
+void InitCamera(void);		//初期化処理
+void UninitCamera(void);	//終了処理
+void UpdateCamera(void);	//更新処理
+void SetCamera(void);		//設定処理
+void MoveCamera(void);		//カメラの移動処理
 Camera *GetCamera(void); //カメラの取得
 
 #endif
