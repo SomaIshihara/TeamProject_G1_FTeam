dc82115780eec46e633e1248dae7ec5555f6803d
Soma Ishihara <221008@st.yoshida-g.ac.jp>
2023-01-18 12:09:22 +0900

    game.cppにコントローラーの使用チェック追加 player.cppにコントローラーを使用しての操作追加

--- game.cpp
+++ game.cpp
@@ -20,8 +20,15 @@ Author:平澤詩苑
 #include "pause.h"
 //#include "sound.h"
 
+//プロト
+
+int SetUsePlayer(void);
+void CheckUsePlayer(void);
+
 //グローバル変数宣言
-bool					g_bPause = false;				// ポーズ
+bool g_bPause = false;				// ポーズ
+bool g_abUsePlayer[MAX_USE_GAMEPAD];//使用しているプレイヤー（接続チェックに使用）
+bool g_bDisconnectPlayer;			//接続が切れたことを確認する
 
 //------------------------------------------------
 //				ゲームの初期化処理
@@ -39,6 +46,7 @@ void InitGame(void)
 	InitPause();		// ポーズ画面の初期化処理
 
 	g_bPause = false;	// ポーズの初期化
+	g_bDisconnectPlayer = false;	//正常にコントローラーが接続されている状態とする
 
 						//ゲームBGM開始
 	//PlaySound(SOUND_LABEL_GAMEBGM);
@@ -137,4 +145,47 @@ void DrawGame(void)
 		DrawPause();		//ポーズ画面描画処理
 	}
 
+}
+
+//------------------------------------------------
+//		使用するプレイヤーの設定
+//		Author:石原颯馬
+//		返り値:使用されている（接続されている）コントローラーの数
+//		Memo:InitGameで呼び出し
+//------------------------------------------------
+int SetUsePlayer(void)
+{
+	int nUsePlayer = 0;
+	for (int nCntPlayer = 0; nCntPlayer < MAX_USE_GAMEPAD; nCntPlayer++)
+	{
+		if (GetUseGamepad(nCntPlayer) == true)
+		{//使用されている
+			g_abUsePlayer[nCntPlayer] = true;
+			nUsePlayer++;	//コントローラーの数追加
+		}
+		else
+		{//使用されていない
+			g_abUsePlayer[nCntPlayer] = false;
+		}
+	}
+	return nUsePlayer;	//使用されているコントローラーの数を返す
+}
+
+//------------------------------------------------
+//		使用しているプレイヤーの接続チェック
+//		Author:石原颯馬
+//		Memo:UpdateGame内でポーズ中でも呼び出しすること
+//------------------------------------------------
+void CheckUsePlayer(void)
+{
+	for (int nCntPlayer = 0; nCntPlayer < MAX_USE_GAMEPAD; nCntPlayer++)
+	{//対応コントローラー分繰り返す
+		if (g_abUsePlayer[nCntPlayer] == true && g_abUsePlayer[nCntPlayer] != GetUseGamepad(nCntPlayer))
+		{//切断を検知
+			g_bPause = true;				//強制的にポーズ状態にする
+			g_bDisconnectPlayer = true;		//切断されたことにする
+			return;	//関数終了
+		}
+	}
+	g_bDisconnectPlayer = false;	//for文が正常に終了したら問題ない
 }
