d20075819c09dccb1a13d21efeba94d380454d06
ShionHirasawa <221053@st.yoshida-g.ac.jp>
2023-01-18 10:30:20 +0900

    シリンダーの半径変更

--- meshcylinder.h
+++ meshcylinder.h
@@ -7,14 +7,11 @@
 #ifndef _MESHCYLONDER_H_	//このマクロ定義がされてなかったら
 #define _MESHCYLONDER_H_	//2重インクルード防止のマクロを定義する
 
-#include"main.h"
-
 //マクロ
-#define MESHSYLINDER_WIDTH		(500.0f)		//背景の広さ
-#define MESHSYLINDER_HEIGHT		(500.0f)		//背景の高さ
-#define MESHSYLINDER_SPLIT		(32)			//背景の頂点数
-#define MESHSYLINDER_TEX_RESOLUTION		(3.0f)	//背景の解像度
-
+#define MESHSYLINDER_WIDTH				(2000.0f)	//背景の広さ
+#define MESHSYLINDER_HEIGHT				(500.0f)	//背景の高さ
+#define MESHSYLINDER_SPLIT				(32)		//背景の頂点数
+#define MESHSYLINDER_TEX_RESOLUTION		(3.0f)		//背景の解像度
 
 //プロトタイプ宣言
 void InitMeshCylinder(void);
