bf2bde175800db219ad9c02d74b3907cc412599e
SyuukiIida <221070@st.yoshida-g.ac.jp>
2023-02-06 10:48:36 +0900

    effect色設定変更&アイテムcpp調整

--- item.h
+++ item.h
@@ -13,9 +13,10 @@ typedef struct
 	D3DXVECTOR3 pos;			//位置
 	D3DXVECTOR3 move;			//位置
 	D3DXVECTOR3 rot;			//角度
-	bool        buse;			//使用しているかどうか
+	int			RespawnDelay;	//リスポーンするまでの時間
 	int			DespawnLimit;	//消えるリミット
 	float		fAlpha;			//アルファ値
+	bool        buse;			//使用しているかどうか
 }Item;
 
 //プロトタイプ宣言
@@ -23,6 +24,7 @@ void InitItem(void);
 void UninitItem(void);
 void UpdateItem(void);
 void DrawItem(void);
+void SetItem(void);
 void CollisionIP(int nPlayerNum);//アイテムとプレイヤーの当たり判定
 
 #endif
