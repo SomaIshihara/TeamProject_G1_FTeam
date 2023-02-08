85db43696d8d7953af9432eebf6d910ec9630251
ShionHirasawa <221053@st.yoshida-g.ac.jp>
2023-02-08 10:59:54 +0900

    チャージシリンダー追加

---
+++ charge_cylinder.h
@@ -0,0 +1,22 @@
+//--------------------------------------------------------------------------------------------------------
+//
+//メッシュのチャージシリンダー[chargecylinder.h]
+//Author:平澤詩苑　飯田洲暉
+//
+//--------------------------------------------------------------------------------------------------------
+#ifndef _CHARGECYLINDER_H_	//このマクロ定義がされてなかったら
+#define _CHARGECYLINDER_H_	//2重インクルード防止のマクロを定義する
+
+//マクロ
+#define CHARGESYLINDER_WIDTH				(2000.0f)	//背景の広さ
+#define CHARGESYLINDER_HEIGHT				(1000.0f)	//背景の高さ
+#define CHARGESYLINDER_SPLIT				(32)		//背景の頂点数
+#define CHARGESYLINDER_TEX_RESOLUTION		(3.0f)		//背景の解像度
+
+//プロトタイプ宣言
+void InitChargeCylinder(void);
+void UninitChargeCylinder(void);
+void UpdateChargeCylinder(void);
+void DrawChargeCylinder(void);
+
+#endif
