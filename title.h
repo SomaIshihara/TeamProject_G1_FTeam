c95d69721c32b2621bd8a43ccf4a93f858cd1495
RyukiFuziwara <221069@st.yoshida-g.ac.jp>
2023-02-15 13:22:45 +0900

    タイトルの選択処理のみ作りました

--- title.h
+++ title.h
@@ -7,11 +7,23 @@ Author:平澤詩苑
 #ifndef _TITLE_H_
 #define _TITLE_H_
 
+//フェードの状態
+typedef enum
+{
+	TITLE_NONE = 0,
+	TITLE_START,	
+	TITLE_EXIT,		
+	TITLE_MAX
+}TITLE;
+
 void InitTitle(void);
 void LoadTitle(void);
 
 void UninitTitle(void);
 void UpdateTitle(void);
 void DrawTitle(void);
+
+void DirecUpTitle(int NumPad);
+void DirecDownTitle(int NumPad);
 
 #endif
