2ac532b3c777af3f13f8f82b1404ae5a27ffaf51
ShionHirasawa <221053@st.yoshida-g.ac.jp>
2023-02-16 12:36:50 +0900

    壁の仕様を変更中

--- wall.cpp
+++ wall.cpp
@@ -4,20 +4,14 @@
 Author:大宮愛羅
 
 ============================================================================================================================================================*/
-
 #include "main.h"
+#include "color.h"
 #include "wall.h"
 #include "player.h"
 #include "input.h"
 
 //マクロ定義
 #define MAX_WALL		(4)		//壁の最大数
-#define MAX_COLOR		(255)	//色の最大値
-#define MAX_STRING		(100)	//文字列の最大数
-#define DISPLAY_ADD		(4)		//表示する分だけ加算
-
-//位置情報
-#define MIN_POS			(0.0f)	//位置の最小値
 
 //法線情報
 #define NOR_WIDTH		(0.0f)	//法線の幅
@@ -27,16 +21,6 @@ Author:大宮愛羅
 //テクスチャ情報
 #define MAX_TEX			(1.0f)	//テクスチャの最大値
 #define MIN_TEX			(0.0f)	//テクスチャの最小値
-
-//CSV関連
-#define START_LE_NUM	(0)		//文字の開始番号
-#define START_CO_NUM	(0)		//列の開始番号
-#define START_LI_NUM	(0)		//行の開始番号
-
-//壁の向き
-#define ANGLE_R			(-0.5f)	//右向き
-#define ANGLE_L			(0.5f)	//左向き
-#define ANGLE_F			(0.0f)	//正面向き
 
 //グローバル変数
 LPDIRECT3DVERTEXBUFFER9 g_pVtxBuffWall = NULL;		//バッファへのポインタ
@@ -76,8 +60,8 @@ void InitWall(void)
 		//頂点座標の設定
 		pVtx[VTX_LE_UP].pos = D3DXVECTOR3(-g_aWall[nCntWall].fWidth, +g_aWall[nCntWall].fHeight, +g_aWall[nCntWall].fDepth);	//左上端
 		pVtx[VTX_RI_UP].pos = D3DXVECTOR3(+g_aWall[nCntWall].fWidth, +g_aWall[nCntWall].fHeight, -g_aWall[nCntWall].fDepth);	//右上端
-		pVtx[VTX_LE_DO].pos = D3DXVECTOR3(-g_aWall[nCntWall].fWidth, MIN_POS, +g_aWall[nCntWall].fDepth);						//左下端
-		pVtx[VTX_RI_DO].pos = D3DXVECTOR3(+g_aWall[nCntWall].fWidth, MIN_POS, -g_aWall[nCntWall].fDepth);						//右下端
+		pVtx[VTX_LE_DO].pos = D3DXVECTOR3(-g_aWall[nCntWall].fWidth, 0.0f, +g_aWall[nCntWall].fDepth);						//左下端
+		pVtx[VTX_RI_DO].pos = D3DXVECTOR3(+g_aWall[nCntWall].fWidth, 0.0f, -g_aWall[nCntWall].fDepth);						//右下端
 
 		//法線ベクトルの設定
 		pVtx[VTX_LE_UP].nor = D3DXVECTOR3(NOR_WIDTH, NOR_HEGHT, NOR_DEPTH);		//左上端
@@ -86,10 +70,10 @@ void InitWall(void)
 		pVtx[VTX_RI_DO].nor = D3DXVECTOR3(NOR_WIDTH, NOR_HEGHT, NOR_DEPTH);		//右下端
 
 		//頂点カラー設定
-		pVtx[VTX_LE_UP].col = D3DCOLOR_RGBA(MAX_COLOR, MAX_COLOR, MAX_COLOR, MAX_COLOR);	//左上端
-		pVtx[VTX_RI_UP].col = D3DCOLOR_RGBA(MAX_COLOR, MAX_COLOR, MAX_COLOR, MAX_COLOR);	//右上端
-		pVtx[VTX_LE_DO].col = D3DCOLOR_RGBA(MAX_COLOR, MAX_COLOR, MAX_COLOR, MAX_COLOR);	//左下端
-		pVtx[VTX_RI_DO].col = D3DCOLOR_RGBA(MAX_COLOR, MAX_COLOR, MAX_COLOR, MAX_COLOR);	//右下端
+		pVtx[VTX_LE_UP].col = RGBA_WHITE;	//左上端
+		pVtx[VTX_RI_UP].col = RGBA_WHITE;	//右上端
+		pVtx[VTX_LE_DO].col = RGBA_WHITE;	//左下端
+		pVtx[VTX_RI_DO].col = RGBA_WHITE;	//右下端
 
 		//テクスチャ座標の設定
 		pVtx[VTX_LE_UP].tex = D3DXVECTOR2(MIN_TEX, MIN_TEX);				//左上端
@@ -97,6 +81,7 @@ void InitWall(void)
 		pVtx[VTX_LE_DO].tex = D3DXVECTOR2(MIN_TEX, MAX_TEX);				//左下端
 		pVtx[VTX_RI_DO].tex = D3DXVECTOR2(MAX_TEX, MAX_TEX);				//右下端
 	}
+
 	//頂点バッファをアンロックする
 	g_pVtxBuffWall->Unlock();
 }
@@ -137,6 +122,9 @@ void DrawWall(void)
 	//頂点フォーマットの設定
 	pDevice->SetFVF(FVF_VERTEX_3D);
 
+	//両面カリングをON
+	pDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+
 	for (int nCntWall = 0; nCntWall < MAX_WALL; nCntWall++)
 	{
 		if (g_aWall[nCntWall].bUse == true)
@@ -162,24 +150,21 @@ void DrawWall(void)
 			pDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, nCntWall * VTX_MAX, 2);
 		}
 	}
+
+	//通常カリングにする
+	pDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
 }
 
 //壁を読み込む
 void LoadWall(void)
 {
-	int nLetter = START_LE_NUM;	//文字
-	int nColumn = START_CO_NUM;	//列
-	int nLine = START_LI_NUM;	//行
-	char aData[MAX_STRING];		//文字列
+	int nLetter = 0;		//文字
+	int nColumn = 0;		//列
+	int nLine = 0;			//行
+	char aData[MAX_MOJI];	//文字列
 
 	//ファイルポインタ
 	FILE *pFile;
-
-	//ポインタを設定
-	VERTEX_3D *pVtx;
-
-	//頂点バッファをロックし頂点情報へのポインタを取得
-	g_pVtxBuffWall->Lock(0, 0, (void **)&pVtx, 0);
 
 	//バッファの初期化
 	memset(aData, 0, sizeof(aData));
@@ -215,6 +200,7 @@ void LoadWall(void)
 				}
 			}
 
+			//読み込んだ文字列を変数に代入
 			switch (nColumn)
 			{
 				//種類
@@ -225,56 +211,37 @@ void LoadWall(void)
 				//壁の向き
 			case WALLCASE_WALLANGLE:
 				g_aWall[nLine].wallAngle = (WALLANGLE)atoi(aData);
-
-				switch (g_aWall[nLine].wallAngle)
-				{
-					//左向き
-				case WALLANGLE_LEFT:
-					g_aWall[nLine].rot.y = D3DX_PI * ANGLE_L;
-					break;
-
-					//右向き
-				case WALLANGLE_RIGHT:
-					g_aWall[nLine].rot.y = D3DX_PI * ANGLE_R;
-					break;
-
-					//後向き
-				case WALLANGLE_BACK:
-					g_aWall[nLine].rot.y = D3DX_PI;
-					break;
-
-					//前向き
-				case WALLANGLE_FRONT:
-					g_aWall[nLine].rot.y = ANGLE_F;
-					break;
-				}
-
 				//処理を抜ける
 				break;
 
 				//幅
 			case WALLCASE_WIDTH:
-				g_aWall[nLine].fWidth = (float)atof(aData);
+				g_aWall[nLine].fWidth = atof(aData);
 				break;
 
 				//高さ
 			case WALLCASE_HEIGHT:
-				g_aWall[nLine].fHeight = (float)atof(aData);
+				g_aWall[nLine].fHeight = atof(aData);
+				break;
+
+				//奥行き
+			case WALLCASE_DEPTH:
+				g_aWall[nLine].fDepth = atof(aData);
 				break;
 
 				//X座標
 			case WALLCASE_POSX:
-				g_aWall[nLine].pos.x = (float)atof(aData);
+				g_aWall[nLine].pos.x = atof(aData);
 				break;
 
 				//Y座標
 			case WALLCASE_POSY:
-				g_aWall[nLine].pos.y = (float)atof(aData);
+				g_aWall[nLine].pos.y = atof(aData);
 				break;
 
 				//Z座標
 			case WALLCASE_POSZ:
-				g_aWall[nLine].pos.z = (float)atof(aData);
+				g_aWall[nLine].pos.z = atof(aData);
 				break;
 			}
 
@@ -286,29 +253,18 @@ void LoadWall(void)
 
 			if (nLetter == '\n')
 			{//読み込んだ文字が改行だった時、
-			 // 使用している状態にする
-				g_aWall[nLine].bUse = true;
+			 
+				g_aWall[nLine].rot = ZERO_SET;	// 向き初期化
+				g_aWall[nLine].bUse = true;		// 使用している状態にする
 
-				//頂点座標の設定
-				pVtx[VTX_LE_UP].pos = D3DXVECTOR3(-g_aWall[nLine].fWidth, +g_aWall[nLine].fHeight, +g_aWall[nLine].fDepth);	//左上端
-				pVtx[VTX_RI_UP].pos = D3DXVECTOR3(+g_aWall[nLine].fWidth, +g_aWall[nLine].fHeight, -g_aWall[nLine].fDepth);	//右上端
-				pVtx[VTX_LE_DO].pos = D3DXVECTOR3(-g_aWall[nLine].fWidth, MIN_POS, +g_aWall[nLine].fDepth);					//左下端
-				pVtx[VTX_RI_DO].pos = D3DXVECTOR3(+g_aWall[nLine].fWidth, MIN_POS, -g_aWall[nLine].fDepth);					//右下端
-
-				pVtx += DISPLAY_ADD;
-
-				//列数を初期化して行数を増やす
-				nColumn = START_CO_NUM;
-				nLine++;
+				nColumn = 0;					// 列数を初期化
+				nLine++;						// 行数を増やす
 			}
 		}
 
 		//ファイルを閉じる
 		fclose(pFile);
 	}
-
-	//頂点バッファをアンロックする
-	g_pVtxBuffWall->Unlock();
 }
 
 //壁との当たり判定
--- wall.h
+++ wall.h
@@ -33,6 +33,7 @@ typedef enum
 	WALLCASE_WALLANGLE,
 	WALLCASE_WIDTH,
 	WALLCASE_HEIGHT,
+	WALLCASE_DEPTH,
 	WALLCASE_POSX,
 	WALLCASE_POSY,
 	WALLCASE_POSZ,
@@ -42,17 +43,15 @@ typedef enum
 //壁構造体の定義
 typedef struct
 {
-	D3DXVECTOR3 pos; //壁の位置
-	D3DXVECTOR3 rot; //壁の向き
-	WALLANGLE wallAngle;
-	int nType; //壁の種類	
-	float fWidth; //壁の幅
-	float fHeight; //壁の高さ
-	float fDepth; //壁の奥行き
-	bool bUse; //壁を使用しているかどうか
+	D3DXVECTOR3 pos;		//壁の位置
+	D3DXVECTOR3 rot;		//壁の向き
+	WALLANGLE wallAngle;	//向きの種類
+	int nType;				//壁の種類	
+	float fWidth;			//壁の幅
+	float fHeight;			//壁の高さ
+	float fDepth;			//壁の奥行き
+	bool bUse;				//壁を使用しているかどうか
 } Wall;
-
-
 
 //プロトタイプ宣言
 void InitWall(void); //初期化処理
@@ -60,7 +59,6 @@ void UninitWall(void); //終了処理
 void UpdateWall(void); //更新処理
 void DrawWall(void); //描画処理
 void LoadWall(void);
-//void SetWall()
 bool CollisionWall(D3DXVECTOR3 *pPos, D3DXVECTOR3 *pPosOld, D3DXVECTOR3 *pMove, float fWidth, float fHeight);
 
 #endif
